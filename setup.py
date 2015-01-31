7f77d7531b29015a2c5e55780234ebd457b47ba5
Ryan Nelson <rnelsonchem@gmail.com>
2015-01-31 16:41:05 -0500

    Missed a comma in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-    ]
+    ],
 
     keywords = "DBF CSV Pandas SQLalchemy PyTables DataFrame SQL HDF",
 
