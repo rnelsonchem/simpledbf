422b8e70c2a8c6b89879d75a4b78f4fc90c5e4d7
Ryan Nelson <rnelsonchem@gmail.com>
2015-01-31 08:16:00 -0500

    Updated setup.py/.gitignore for dist
    Made a couple of changes to setup.py for my first release on PyPI. Added some
    of the build related stuff to .gitignore.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name = "simpledbf",
     version = "0.1.0",
 
     description = "A simple DBF file converter for Python3",
     url = "https://github.com/rnelsonchem/simpledbf",
+    long_description = long_description,
 
     author = "Ryan Nelson",
     author_email = "rnelsonchem@gmail.com",
